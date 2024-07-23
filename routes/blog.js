12470a02d022f3d53bb43c1c5dd19dc1d4d5a41f
Charlie <charlieedoherty@gmail.com>
2024-07-23 01:08:30 -0500

    Change date display

--- routes/blog.js
+++ routes/blog.js
@@ -46,7 +46,18 @@ router.get("/posts/:id", async function (req, res) {
         return res.status(404).render("404")
     }
 
-    return res.render("post-detail", { post: posts[0] })
+    const postData = {
+        ...posts[0],
+        date: posts[0].date.toISOString(),
+        readableDate: posts[0].date.toLocaleDateString("en-US", {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric"
+        })
+    }
+
+    return res.render("post-detail", { post: postData })
 })
 
 module.exports = router
