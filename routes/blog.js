84b1bb423cac036d24aebcf0b69bf18b0d066af4
Charlie <charlieedoherty@gmail.com>
2024-07-22 23:42:31 -0500

    Display posts under "All Posts"

--- routes/blog.js
+++ routes/blog.js
@@ -3,17 +3,32 @@ const db = require("../data/database.js")
 
 const router = express.Router()
 
-router.get("/", function(req, res) {
+router.get("/", function (req, res) {
     res.redirect("/posts")
 })
 
-router.get("/posts", function(req, res) {
-    res.render("posts-list")
+router.get("/posts", async function (req, res) {
+    const query = `SELECT posts.*, authors.name AS author_name FROM blog.posts 
+        INNER JOIN blog.authors ON blog.posts.author_id = blog.authors.id
+        `
+    const [posts] = await db.query(query)
+    res.render("posts-list", { posts: posts })
 })
 
-router.get("/new-post", async function(req, res) {
-    const [ authors ] = await db.query("SELECT * FROM authors")
-    res.render("create-post", {authors: authors})
+router.get("/new-post", async function (req, res) {
+    const [authors] = await db.query("SELECT * FROM authors")
+    res.render("create-post", { authors: authors })
+})
+
+router.post("/posts", async function (req, res) {
+    const data = [
+        req.body.title,
+        req.body.summary,
+        req.body.content,
+        req.body.author
+    ]
+    await db.query("INSERT INTO posts (title, summary, body, author_id) VALUES (?)", [data])
+    res.redirect("/posts")
 })
 
 module.exports = router
