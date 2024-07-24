b83bb5155dde172dc566a0327cd6895cd476fea5
Charlie <charlieedoherty@gmail.com>
2024-07-24 17:11:58 -0500

    Display comments

--- public/scripts/comments.js
+++ public/scripts/comments.js
@@ -1,11 +1,32 @@
 const loadCommentsButton = document.getElementById("comments-button")
+const commentsSectionElement = document.getElementById("comments")
+
+function createComments(comments) {
+    const commentsListElement = document.createElement("ol")
+
+    for (const comment of comments) {
+        const commentElement = document.createElement("li")
+        commentElement.innerHTML = `
+            <article class="comment-item">
+                <h2>${comment.title}</h2>
+                <p>${comment.text}</p>
+            </article>
+        `
+        commentsListElement.appendChild(commentElement)
+    }
+
+    return commentsListElement
+}
 
 async function fetchComments() {
     const postId = loadCommentsButton.dataset.postid
     const response = await fetch(`/posts/${postId}/comments`)
     const responseData = await response.json()
 
-    createComments()
+    commentsList = createComments(responseData)
+
+    commentsSectionElement.innerHTML = ""
+    commentsSectionElement.appendChild(commentsList)
 }
 
 loadCommentsButton.addEventListener("click", fetchComments)
